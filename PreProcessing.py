c86dd86d7fee7ceed393263eb09fdce161035f75
itsjjaavii <javiera.penav@usm.cl>
2024-11-15 16:47:13 -0300

    comentarios

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,42 +1,47 @@
+# Pre-procesamiento imágenes RGB 
+# Autor: Javiera Peña
 import cv2
 
-# Cargar la imagen RGB 
+# Cargar la imagen RGB -> imagen cargada como matriz de 3 canales 
 imagen_rgb = cv2.imread('InputFiles/RGB.jpg')
-gray_image = cv2.cvtColor(imagen_rgb, cv2.COLOR_BGR2GRAY) 
+
+gray_image = cv2.cvtColor(imagen_rgb, cv2.COLOR_BGR2GRAY) # se convierte la imagen BGRA a escala de grises para poder trabajar con filtros 
+
+# aplicación de filtros que reducen el ruido para mejorar la detección de contornos
 box_blur = cv2.blur(gray_image,(4,4))
 gaussian = cv2.GaussianBlur(gray_image,(7,7),0)
 
-new_imagergb = imagen_rgb.copy()
-cv2.imshow('imagen',new_imagergb) # nueva imagen para el recorte
+new_imagergb = imagen_rgb.copy() # nueva imagen para el recorte -> esto se hace para no modificar la imagen original en caso de necesitarla
+#bcv2.imshow('imagen',new_imagergb) 
 
-_,threshold_image = cv2.threshold(gaussian, 180, 255, cv2.THRESH_BINARY)
+_,threshold_image = cv2.threshold(gaussian, 180, 255, cv2.THRESH_BINARY) # aplicar un umbral binario a la imagen Guassiana
 contorno,_ = cv2.findContours(threshold_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contornos_dibujados = cv2.drawContours(imagen_rgb,contorno,-1,(0,255,0),3)  # crear los contornos en imagen rgb
 
-# Ordenar los contornos por área para seleccionar los paneles más grandes
+# ordenar los contornos por área para seleccionar los paneles más grandes
 contours = sorted(contorno, key=cv2.contourArea, reverse=True)
 
-# Inicializar lista de recortes de los paneles
+# inicializar lista de recortes de los paneles
 panel_crops = []
 
-# Iterar sobre los dos contornos más grandes (asumimos que corresponden a los paneles grandes)
+# iterar sobre los dos contornos más grandes (asumimos que corresponden a los paneles grandes)
 for i in range(2):
-    # Obtener el rectángulo delimitador de cada contorno
+    # obtener el rectángulo delimitador de cada contorno
     x, y, w, h = cv2.boundingRect(contours[i])
     
-    # Recortar la imagen usando las coordenadas del rectángulo
+    # recortar la imagen usando las coordenadas del rectángulo
     panel_crop = new_imagergb[y:y+h, x:x+w] # se usa la nueva imagen para recortar 
     panel_crops.append(panel_crop)
     
-    # Guardar cada recorte individualmente
+    # guardar cada recorte individualmente
     cv2.imwrite(f'panel_recorte_{i+1}.png', panel_crop)
 
-# Mostrar el resultado final en pantalla para verificación
+# mostrar el resultado final en pantalla para verificación
 for idx, panel in enumerate(panel_crops):
     cv2.imshow(f'Panel {idx+1}', panel)
 
 
-# Mostrar la imagen original con los contornos
+# mostrar la imagen original con los contornos
 # cv2.imshow('cont', contornos_dibujados)
 
 
